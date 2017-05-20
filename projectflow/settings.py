369b0e9fd3e0cf21344a3dc8dcf222675e2e666a
johnny <chengyichin@gmail.com>
2017-05-20 23:37:48 +0800

    Changed the HTML for the site.

--- projectflow/settings.py
+++ projectflow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '33=ne)k$2mvcm*_-osgt=hb5soi31ny5yasx-$=78!jg!0sxc$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Hong_Kong'
 
 USE_I18N = True
 
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
